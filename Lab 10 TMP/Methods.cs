165fe48c28a1485607c14b3f3247240fa88347fc
Dear Ilya <Dear Ilya@DESKTOP-EGLS4E4>
2024-04-12 16:31:34 +0500

    Добавьте файлы проекта.

---
+++ Lab 10 TMP/Methods.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Lab_10_TMP
+{
+    class Methods
+    {
+        public static Random random = new Random();
+        public static Mutex mutex = new Mutex();
+        public static Semaphore semaphore = new Semaphore(3, 3);
+        public static SemaphoreSlim semaphoreSlim = new SemaphoreSlim(3);
+        public static SpinLock spinLock = new SpinLock();
+        public static object locker = new object();
+        public ManualResetEvent manualResetEvent = new ManualResetEvent(false);
+
+
+        public void MutexPrint(string sum)
+        {
+            //manualResetEvent.WaitOne();
+            for (int i = 0; i < 500; i++)
+            {
+                //Thread.Sleep(random.Next(500));
+                mutex.WaitOne();
+                Console.WriteLine(sum);
+                mutex.ReleaseMutex();
+            }
+        }
+
+        public void SemaphorePrint(string sum)
+        {
+            //manualResetEvent.WaitOne();
+            for (int i = 0; i < 500; i++)
+            {
+                //Thread.Sleep(random.Next(500));
+                semaphore.WaitOne();
+                Console.WriteLine(sum);
+                semaphore.Release();
+            }
+        }
+
+        public void SemaphoreSlimPrint(string sum)
+        {
+            //manualResetEvent.WaitOne();
+            for (int i = 0; i < 500; i++)
+            {
+                //Thread.Sleep(random.Next(500));
+                semaphoreSlim.Wait();
+                Console.WriteLine(sum);
+                semaphoreSlim.Release();
+            }
+        }
+
+        public void MonitorPrint(string sum)
+        {
+            //manualResetEvent.WaitOne();
+            Monitor.Enter(locker);
+            for (int i = 0; i < 500; i++)
+            {
+                //Thread.Sleep(random.Next(500));
+                Console.WriteLine(sum);
+            }
+            Monitor.Exit(locker);
+        }
+
+        public void SpinlockPrint(string sum)
+        {
+            //manualResetEvent.WaitOne();
+            bool flag = false;
+
+                spinLock.Enter(ref flag);
+                for (int i = 0; i < 500; i++)
+                {
+                    //Thread.Sleep(random.Next(500));
+                    Console.WriteLine(sum);
+                }
+
+                if (flag)
+                {
+                    spinLock.Exit();
+                }
+            
+        }
+
+        public void Menu()
+        {
+            Console.WriteLine("Введите");
+            Console.WriteLine("1. Для использования мьютекса.");
+            Console.WriteLine("2. Для использования семафора.");
+            Console.WriteLine("3. Для использования семафорСлим.");
+            Console.WriteLine("4. Для использования монитора.");
+            Console.WriteLine("5. Для завершения спинлока.");
+            Console.Write("Ввод: ");
+        }
+    }
+}
---
+++ Lab 10 TMP/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using BenchmarkDotNet;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Characteristics;
+using Microsoft.Diagnostics.Symbols;
+using Microsoft.Diagnostics.Tracing.Parsers.Clr;
+using BenchmarkDotNet.Running;
+
+namespace Lab_10_TMP
+{
+    [MemoryDiagnoser]
+    public class Program
+    {
+        public static Thread thread1, thread2, thread3;
+        static Methods mt = new Methods();
+
+
+        [Benchmark]
+        public void t1b()
+        {
+            mt.MutexPrint("###");
+        }
+
+        [Benchmark]
+        public void t2b()
+        {
+            mt.SemaphorePrint("$$$");
+        }
+
+        [Benchmark]
+        public void t3b()
+        {
+
+            mt.SemaphoreSlimPrint("@@@");
+        }
+
+        [Benchmark]
+        public void t4b()
+        {
+
+            mt.MonitorPrint("@@@");
+        }
+
+        [Benchmark]
+        public void t5b()
+        {
+
+            mt.SpinlockPrint("@@@");
+        }
+
+        static void Main(string[] args)
+        {
+            //mt.Menu();
+            //string ch = Console.ReadLine();
+
+            //switch (ch)
+            //{
+            //    case "1":
+            //        thread1 = new Thread(() => mt.MutexPrint("###"));
+            //        thread2 = new Thread(() => mt.MutexPrint("$$$"));
+            //        thread3 = new Thread(() => mt.MutexPrint("@@@"));
+            //        break;
+            //    case "2":
+            //        thread1 = new Thread(() => mt.SemaphorePrint("###"));
+            //        thread2 = new Thread(() => mt.SemaphorePrint("$$$"));
+            //        thread3 = new Thread(() => mt.SemaphorePrint("@@@"));
+            //        break;
+            //    case "3":
+            //        thread1 = new Thread(() => mt.SemaphoreSlimPrint("###"));
+            //        thread2 = new Thread(() => mt.SemaphoreSlimPrint("$$$"));
+            //        thread3 = new Thread(() => mt.SemaphoreSlimPrint("@@@"));
+            //        break;
+            //    case "4":
+            //        thread1 = new Thread(() => mt.MonitorPrint("###"));
+            //        thread2 = new Thread(() => mt.MonitorPrint("$$$"));
+            //        thread3 = new Thread(() => mt.MonitorPrint("@@@"));
+            //        break;
+            //    case "5":
+            //        thread1 = new Thread(() => mt.SpinlockPrint("###"));
+            //        thread2 = new Thread(() => mt.SpinlockPrint("$$$"));
+            //        thread3 = new Thread(() => mt.SpinlockPrint("@@@"));
+            //        break;
+            //    default:
+            //        Console.WriteLine("Такой команды нет.");
+            //        break;
+            //}
+
+            //Console.WriteLine("Для запуска потоков нажмите Enter");
+            //Console.ReadLine();
+            //mt.manualResetEvent.Set();
+            //thread1.Start();
+            //thread2.Start();
+            //thread3.Start();
+            BenchmarkRunner.Run<Program>();
+        }
+    }
+}
